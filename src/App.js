5c5357953ad5e4ffb7f9de86f2c22ff9a110cf63
Steven Cochrane <stevenjcochrane@gmail.com>
2022-07-27 22:49:17 -0400

    hooked data-fetching-playground package in using yalc and updated App.js to use new package for SPA browser CSR. Needs further work to test out server components and SSR

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+import testing from "data-fetching-playground";
+
 function App() {
+  testing();
+
   return (
     <div className="App">
       <header className="App-header">
